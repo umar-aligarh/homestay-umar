5ffd700a88163382a1146dabc5fc665ef6daa476
Umar <uk.umarkhan1999@gamil.com>
2023-06-01 16:41:06 +0530

    main.js updated

--- server/main.js
+++ server/main.js
@@ -2,7 +2,15 @@ const express = require("express");
 const app = express();
 
 
-const userRoute = require()
+const usersRouter = require('./routes/userRoutes');
+const bookingsRouter = require('./routes/bookingRoutes');
+const roomsRouter = require('./routes/roomRoutes');
+const transactionsRouter = require('./routes/transactionRoutes');
+
+app.use('/users', usersRouter);
+app.use('/bookings', bookingsRouter);
+app.use('/rooms', roomsRouter);
+app.use('/transactions', transactionsRouter);
 
 //ddd
 app.listen(3000,()=>{
