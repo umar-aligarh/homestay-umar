b27b5b8ec1c48234f5421747d8be6af586bc9bc9
Umar <uk.umarkhan1999@gamil.com>
2023-06-01 16:30:59 +0530

    user routes added

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const User = require('../models/userAccountsModel');
+
+
+router.route('/add').post((req, res) => {
+    const mob = req.body.mobNo;
+
+    const newUser = new User({
+       _id: mob
+    });
+  
+    newUser.save()
+    .then(() => res.json('User added!'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
